6291f86f0722cd61bc45bf0d69af828ccf0017ab
Adam Boyer <alexander.boyer75@gmail.com>
2022-03-12 10:00:25 -0600

    Adding dropdown animation and some colors

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ const hamburgerIcon = document.getElementById("hamburger");
 const toggleOpenHamburger = () => {
   document.getElementById("hamburger").classList.toggle("open");
   document.getElementById("mobile-nav").classList.toggle("ham-open");
+  document.getElementById("mobile-links").classList.toggle("ham-open");
 };
 hamburgerIcon.addEventListener("click", toggleOpenHamburger);
